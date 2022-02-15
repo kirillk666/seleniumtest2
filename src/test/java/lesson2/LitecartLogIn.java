5b38fafc187329fb217542b16b047388c2c7125c
Kirill <kirienko.k@spb-ifuture.com>
2022-02-15 14:18:24 +0300

    loginTest

---
+++ src/test/java/lesson2/LitecartLogIn.java
@@ -0,0 +1,35 @@
+package lesson2;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LitecartLogIn {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeTest
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void LoginTest() {
+        driver.get("http://localhost/litecart/admin/login.php");
+        driver.findElement(By.cssSelector("input[type=text]")).sendKeys("admin");
+        driver.findElement(By.cssSelector("input[type=password]")).sendKeys("admin");
+        driver.findElement(By.cssSelector(".footer button")).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".fa-sign-out")));
+    }
+    @AfterTest
+        public void stop(){
+            driver.quit();
+            driver=null;
+    }
+}
